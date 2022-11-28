c4e9b65f909a3169e67211ccc30a9c39e735c3ef
mitander <carlmitander@gmail.com>
2022-11-28 23:15:38 +0100

    Add clang-format

--- examples/example.cpp
+++ examples/example.cpp
@@ -1,8 +1,9 @@
 #include "mylib/mylib.h"
 #include <stdio.h>
 
-int main() {
-  printf("hello from example\n");
-  mylib_hello();
-  return 0;
+int main()
+{
+    printf("hello from example\n");
+    mylib_hello();
+    return 0;
 }
